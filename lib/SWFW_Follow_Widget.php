df933dc59f80599e3c9f4c04e5a05df3d4794c19
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-11-13 14:26:01 -0500

    Created init_networks; trying an array of network arrays.

--- lib/SWFW_Follow_Widget.php
+++ lib/SWFW_Follow_Widget.php
@@ -7,6 +7,39 @@ class SWFW_Follow_Widget {
 
     public function __construct() {
 		$this->load_files( '/lib/', array( 'SWFW_Follow_Network' ) );
+
+		$this->init_networks();
+	}
+
+	function init_networks() {
+		$networks = array(
+			array(
+				'key'	=> 'facebook',
+				'name'	=> 'Facebook',
+				'cta'	=> 'Like',
+				'color_primary'	=> '#3A589E',
+				'color_accent'	=> '#314E84',
+				'url'	=> 'xxx.com'
+			),
+			array(
+				'key' => 'tumblr',
+				'name' => 'Tumblr',
+				'cta' => 'Like',
+				'color_primary' => '#39475D',
+				'color_accent' => '#27313F',
+				'url'	=> 'yyy.com'
+			),
+			array(
+				'key' => 'pinterest',
+				'name' => 'Pinterst',
+				'cta' => 'Follow',
+				'color_primary' => '#CC2029',
+				'color_accent' => '#CC#AB1F25',
+				'url'	=> 'zzz.com'
+			)
+		);
+
+		array_map( $networks, 'SWFW_Follow_Network');
 	}
 
 	/**
