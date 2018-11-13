0fd9adf227331a453bf1999ad64349e02cb34c89
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-11-13 12:18:30 -0500

    Create button.php

---
+++ button.php
@@ -0,0 +1,8 @@
+<div class="swfm-follow-button">
+  <div class="swfm-network-icon"></div>
+
+  <div class="swfm-content">
+    <div class="swfm-count"></div>
+    <div class="swfm-cta"></div>
+  </div>
+</div>
