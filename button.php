ecbbb4ce78a4f704983bb6ad977db5fc4fe23ad0
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-11-13 12:44:07 -0500

    Created demo with dummy data.

--- button.php
+++ button.php
@@ -1,8 +1,20 @@
-<div class="swfm-follow-button">
-  <div class="swfm-network-icon"></div>
+<?php
+//* Dummy data. These can just as well be class properties.
+$style = 'square '; // or 'rect-small' or 'rect-large';
+$network = 'facebook';
+$icon = '<i class="swp-facebook"></i>';
+$count = 300;
+$cta = "Follow";
+
+$button = <<<EOT
+<div class="swfm-follow-button $style $network" data-newtork="$network">
+  <div class="swfm-network-icon">$icon</div>
 
   <div class="swfm-content">
-    <div class="swfm-count"></div>
-    <div class="swfm-cta"></div>
+    <div class="swfm-count">$count</div>
+    <div class="swfm-cta">$cta</div>
   </div>
 </div>
+EOT;
+
+echo $button;
