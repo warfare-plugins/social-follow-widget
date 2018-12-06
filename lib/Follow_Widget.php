a128478d3de3b4e71b9c31fcc98834c9e8c8582d
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-12-06 14:27:28 -0500

    Created class Follow_Widget.

---
+++ lib/Follow_Widget.php
@@ -0,0 +1,17 @@
+<?php
+
+class Follow_Widget extends SWP_Maybe_Widget {
+	function __construct() {
+		parent::__construct();
+	}
+
+	function render_form_HTML( $settings ) {
+		$html = '';
+        return $html;
+	}
+
+	function render_widget_HTML( $args, $settings ) {
+		$html = '';
+        return $html;
+	}
+}
