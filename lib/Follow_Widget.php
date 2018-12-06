138be1edf6d0bc6dae1feee31c8b900b500a3b7d
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-12-06 14:54:14 -0500

    Filling out constructor, added dummy content for generate methods.

--- lib/Follow_Widget.php
+++ lib/Follow_Widget.php
@@ -2,16 +2,24 @@
 
 class Follow_Widget extends SWP_Maybe_Widget {
 	function __construct() {
+		$this->key = 'social_warfare_follow_widget';
+
+		$args = array(
+			'name'	=> 'Social Warfare Follow Widget'
+		);
+
 		parent::__construct();
 	}
 
 	function generate_form_HTML( $settings ) {
-		$html = '';
+		$html = '<h1>I am a form!</h1>';
+		$html .= '<form></form>';
         return $html;
 	}
 
 	function generate_widget_HTML( $args, $settings ) {
-		$html = '';
+		$html = '<h1>I am a widget!</h1>';
+		$html .= '<pre>widget</pre>';
         return $html;
 	}
 }
