e2527756cde5982e37a54eecbcd95f90eb49d4f6
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-12-17 15:03:37 -0500

    Deleted copypasta methods.

--- lib/SWFW_Follow_Network.php
+++ lib/SWFW_Follow_Network.php
@@ -135,19 +135,11 @@ class SWFW_Follow_Network {
 		add_filter( 'swfw_follow_networks', array( $this, 'register_self' ) );
 	}
 
-	function generate_url() {
-		return str_replace( 'swfw_username', $this->username, $this->url);
-	}
-
-	function is_active() {
-		return !empty( $this->username );
-	}
-
 	/**
 	 * A method to add this network object to the globally accessible array.
 	 *
 	 * @since  3.0.0 | 06 APR 2018 | Created
-	 * @hook   filter| swp_follow_networks  
+	 * @hook   filter| swp_follow_networks
 	 * @param  array $networks All of the created Social Follow Network classes.
 	 * @return array $networks With `$this` network in the array.
 	 * @access public
@@ -157,134 +149,16 @@ class SWFW_Follow_Network {
         return array_merge( $networks, array( $this ) );
 	}
 
-	/**
-	 * A function to run when the object is instantiated.
-	 *
-	 * @since  3.0.0 | 06 APR 2018 | Created
-	 * @param  none
-	 * @return none
-	 * @access public
-	 *
-	 */
-	public function init_social_network() {
 
+	function generate_url() {
+		return str_replace( 'swfw_username', $this->username, $this->url);
+	}
+
+	function is_active() {
+		return !empty( $this->username );
 	}
 
 
-	/**
-	 * A method for providing the object with a name.
-	 *
-	 * @since 3.0.0 | 05 APR 2018 | Created
-	 * @param string $value The name of the object.
-	 * @return object $this Allows chaining of methods.
-	 * @access public
-	 *
-	 */
-	public function set_name( $value ) {
-
-    }
-
-
-	/**
-	 * A method for updating this network's default property.
-	 *
-	 * @since 3.0.0 | 05 APR 2018 | Created
-	 * @param bool $value The default status of the network.
-	 * @return object $this Allows chaining of methods.
-	 * @access public
-	 *
-	 */
-	public function set_default( $value ) {
-
-	}
-
-
-	/**
-	 * A method for updating this network's key property.
-	 *
-	 * @since 3.0.0 | 05 APR 2018 | Created
-	 * @param string $value The key for the network.
-	 * @return object $this Allows chaining of methods.
-	 * @access public
-	 *
-	 */
-	public function set_key( $value ) {
-
-	}
-
-
-	/**
-	 * A method for updating this network's premium property.
-	 *
-	 * @since 3.0.0 | 05 APR 2018 | Created
-	 * @param string $value A string corresponding to the key of the dependant premium addon.
-	 * @return object $this Allows chaining of methods.
-	 * @access public
-	 *
-	 */
-	public function set_premium( $value ) {
-
-	}
-
-
-	/**
-	 * A method to return the 'active' status of this network.
-	 *
-	 * @since 3.0.0 | 06 APR 2018 | Created
-	 * @param none
-	 * @return bool
-	 * @access public
-	 *
-	 */
-	// is_active func
-
-
-	/**
-	 * A method to set the 'active' status of this network.
-	 *
-	 * @since 3.0.0 | 06 APR 2018 | Created
-	 * @param none
-	 * @return none
-	 * @access public
-	 *
-	 */
-	public function set_active_state() {
-
-	}
-
-
-	/**
-	 * A method to save the generated HTML. This allows us to not have to
-	 * run all of the computations every time. Instead, just reuse the HTML
-	 * that was rendered by the method the first time it was created.
-	 *
-	 * @since  3.0.0 | 06 APR 2018 | Created
-	 * @param  string  $html     The string of HTML to save in this property.
-	 * @param  int     $post_id  The ID of the post that this belongs to.
-	 * @return none
-	 * @access public
-	 *
-	 */
-	public function save_html( $html , $post_id ) {
-
-	}
-
-	/**
-	 * Show Share Counts?
-	 *
-	 * A method to determine whether or not share counts need to be shown
-	 * while rendering the HTML for this network's button.
-	 *
-	 * @since  3.0.0 | 06 APR 2018 | Created
-	 * @param  array $array The array of data from the buttons panel.
-	 * @return bool
-	 * @access public
-	 * @TODO Make it accept two parameters, both arrays, $options and $share_counts.
-	 *
-	 */
-	public function is_share_count_shown( $array ) {
-
-	}
 
 
 	/**
@@ -343,97 +217,4 @@ BUTTON;
 
 		return $button;
 	}
-
-
-	/**
-	 *
-	 * Returns a boolean indicateding whether or not to display share counts.
-	 *
-	 * @since  3.0.0 | 18 APR 2018 | Created
-	 * @since  3.3.0 | 24 AUG 2018 | Removed use of $options, calls SWP::Utility instead.
-	 *
-	 * @param  array $share_counts The array of share counts
-	 * @param  array $options  DEPRECATED The array of options from the button panel object.
-	 *
-	 * @return bool  True if share counts should be displayed, else false.
-	 *
-	 */
-	public function are_shares_shown( $share_counts , $options = array()) {
-
-	}
-
-
-	/**
-	 * A method for processing URL's.
-	 *
-	 * This is designed to process the URL that is being shared onto the social
-	 * platorms. It takes care of encoding, UTM parameters, link shortening, etc.
-	 *
-	 * @since  3.0.0 | 06 APR 2018 | Created
-	 * @param  array $array  The array of data from the buttons panel.
-	 * @return string        The processed URL.
-	 *
-	 */
-	public function get_shareable_permalink( $post_data ) {
-	}
-
-
-	/**
-	 * Generate the share link
-	 *
-	 * This is the link that is being clicked on which will open up the share
-	 * dialogue. Thie method is only used for networks that use this exact same pattern.
-	 * For anything that accepts more than just the post permalink as a URL parameter,
-	 * those networks will have to overwrite this method with their own custom method
-	 * in their respective child classes.
-	 *
-	 * @since  3.0.0 | 08 APR 2018 | Created
-	 * @param  array $array The array of information passed in from the buttons panel.
-	 * @return string The generated link
-	 * @access public
-	 *
-	 */
-	public function generate_share_link( $post_data ) {
-
-	}
-
-
-	/**
-	 * Generate the API Share Count Request URL
-	 *
-	 * For most social networks, the api link is unique and this method will need to be
-	 * overwritten in their respective child classes. However, for any networks that do
-	 * not support share counts, having the method here in the parent class will allow
-	 * us to simply use this one without have to write a new one in each child class.
-	 *
-	 * @since  3.0.0 | 08 APR 2018 | Created
-	 * @access public
-	 * @param  string $url The permalink of the page or post for which to fetch share counts
-	 * @return string $request_url The complete URL to be used to access share counts via the API
-	 *
-	 */
-	public function get_api_link( $url ) {
-
-	}
-
-
-	/**
-	 * Parse the response to get the share count
-	 *
-	 * For most social networks, parsing of the API response needs to be a unique method
-	 * that is declared in each network's child class. However, we are including it here
-	 * for all networks that do not support share counts at all. If a network does not
-	 * support share count fetching, then it can just use the method defined here in the
-	 * parent class.
-	 *
-	 * @since  3.0.0 | 08 APR 2018 | Created
-	 * @access public
-	 * @param  string $response The raw response returned from the API request
-	 * @return int $total_activity The number of shares reported from the API
-	 *
-	 */
-	public function parse_api_response( $response ) {
-
-	}
-
 }
