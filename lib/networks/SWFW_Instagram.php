c31e9396c5495408c313bbd9564cf7aa3dd6c4d9
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-08 16:38:51 -0500

    Created SWFW_Instagram.

---
+++ lib/networks/SWFW_Instagram.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * SWFW_Instagram
+ *
+ * This provides an interface for creating a follow button for Instagram.
+ *
+ * @package   social-follow-widget
+ * @copyright Copyright (c) 2019, Warfare Plugins, LLC
+ * @license   GPL-3.0+
+ * @since     1.0.0 | Created
+ *
+ */
+class SWFW_Instagram extends SWFW_Follow_Network {
+
+
+	/**
+	 * Applies network-specific data to the SWFW_Follow_Network
+	 *
+	 * @since 1.0.0 | 03 DEC 2018 | Created.
+	 * @see SWFW_Follow_Network
+	 * @param void
+	 * @return void
+	 */
+	public function __construct() {
+		$network = array(
+			'key' => 'instagram',
+			'name' => 'Instagram',
+			'cta' => 'Follow',
+			'follow_description' => 'Followers',
+			'color_primary' => '#5851db',
+			'color_accent' => '#c13584',
+			'url'	=> 'https://instagram.com/swfw_username'
+		);
+
+		parent::__construct( $network );
+	}
+}
