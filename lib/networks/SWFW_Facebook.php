53a17cb4d58bedac7e32abf0e7433685b959ca7d
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-15 18:57:17 -0500

    Created get_count_request_url().

--- lib/networks/SWFW_Facebook.php
+++ lib/networks/SWFW_Facebook.php
@@ -35,5 +35,54 @@ class SWFW_Facebook extends SWFW_Follow_Network {
 		);
 
 		parent::__construct( $network );
+		$this->get_count_request_url();
+	}
+
+
+	/**
+	 * [get_count_request_url description]
+	 * @return [type] [description]
+	 */
+	public function get_count_request_url() {
+		if ( !$this->auth_helper->has_credentials ) {
+			return false;
+		}
+
+		require_once __DIR__ . '/../SDKs/Facebook/autoload.php';
+
+		// $endpoint = "/{$this->username}/fan_count";
+		$app_id = '2194481457470892';
+		$app_secret = base64_decode('OGQzZmZkYTUzYzBmY2EzNDNhNGQwOTMyZWIwMDYwMzc=');
+		$endpoint = "/{$this->username}/locations";
+
+		$access_token1 = $this->auth_helper->get_access_token();
+		$access_token2 = $app_id.'|'.$this->auth_helper->get_access_token();
+		$access_token3 = $app_id.'|'.$app_secret;
+
+		$fb = new Facebook\Facebook(array(
+			'app_id' => $app_id,
+			'app_secret' => $app_secret,
+			'default_graph_version' => 'v3.20',
+		));
+
+		try {
+
+		  // Returns a `Facebook\FacebookResponse` object
+		  $response = $fb->get('/'. $app_id.'/permissions', $access_token3 );
+		  // $response = $fb->get('/', $access_token3);
+		  echo 'Response from facebook: <pre>';
+		  die(var_dump($response));
+
+		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		  echo 'Graph returned an error: ' . $e->getMessage();
+		  exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		  exit;
+		}
+		$graphNode = $response->getGraphNode();
+		/* handle the result */
+
+		return '';
 	}
 }
