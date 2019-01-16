a5a0c4c1cc1eb9fb4087875ee3d5a0f1f6a0b19a
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-15 19:03:19 -0500

    Added dockblock;

--- lib/SWFW_Cache.php
+++ lib/SWFW_Cache.php
@@ -16,10 +16,17 @@ class SWFW_Cache {
 	}
 
 
+	/**
+	 * Updates the follow counts in the database and the last_updated timestamp.
+	 *
+	 * @param  array $counts The ['network_key' => (int) follow_count] data.
+	 * @return bool  True iff the counts were updated, else false.
+	 * 
+	 */
 	public static function update_follow_counts( $counts ) {
 		$now = time() / DAY_IN_SECONDS;
 
-		SWFW_Utility::update_option( 'counts', $counts );
 		SWFW_Utility::update_option( 'last_updated', $now );
+		return SWFW_Utility::update_option( 'counts', $counts );
 	}
 }
