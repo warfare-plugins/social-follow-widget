55b4b213b9e260e17c3f8995755c1bccbd8f23bf
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-17 14:09:27 -0500

    Only testing Twitter right now.

--- lib/Social_Warfare_Follow_Widget.php
+++ lib/Social_Warfare_Follow_Widget.php
@@ -82,13 +82,13 @@ class Social_Warfare_Follow_Widget extends Social_Warfare_Addon {
 	 */
 	public function init_networks() {
 		$networks = array(
-			'Facebook',
-			'Pinterest',
-			'Reddit',
+			// 'Facebook',
+			// 'Pinterest',
+			// 'Reddit',
 			'Twitter',
-			'Tumblr',
-			'Instagram',
-			'Vimeo'
+			// 'Tumblr',
+			// 'Instagram',
+			// 'Vimeo'
 		);
 
 		$this->load_files( '/lib/networks/', $networks, true );
