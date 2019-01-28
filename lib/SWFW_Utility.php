5b0e14252f60012b953f29c0a8a5c52dbb724a82
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-28 12:34:31 -0500

    Created `save_follow_counts()`, `update_network_count()`.

--- lib/SWFW_Utility.php
+++ lib/SWFW_Utility.php
@@ -6,6 +6,7 @@ class SWFW_Utility {
 	 * The plugins data stored in wp_options.
 	 *
 	 * The contents are network counts and a timestamp.
+	 * $options = array( 'last_updated' => 3481834, 'pinterest' => 1231, 'facebook' => 3851 );
 	 *
 	 * Settings are otherwise already managed by the WP_Widget for each instance
 	 * of Social_Warfare_Follow_Widget.
@@ -36,6 +37,15 @@ class SWFW_Utility {
 	}
 
 
+
+	public static function save_follow_counts() {
+		$updated = self::update_option( 'swfw_options', self::$options );
+		SWFW_Cache::update_cache_timestamp();
+
+		return $updated;
+	}
+
+
 	/**
 	 * Retrieves the associative array of plugin options.
 	 *
@@ -50,15 +60,32 @@ class SWFW_Utility {
 		$options = get_option( 'swfw_options', array() );
 
 		if ( empty( $options ) ) {
-			/**
-			 * Initialize the SWFW options with a timestamp.
-			 *
-			 */
-			$options = array( 'last_updated' => 0 );
+			 //Initialize the SWFW options with a timestamp.
+			$options = array( 'last_updated' => 0  );
 			update_option( 'swfw_options', $options );
 		}
 
 		return self::$options = $options;
+	}
+
+	/**
+	 * Update the local stored value for a network count.
+	 *
+	 * First update all local values, then store the updated array in the database.
+	 *
+	 * @param  string $network	The network whose follow count to update.
+	 * @param  int    $count  	The value of the count.
+	 * @return void
+	 *
+	 */
+	public static function update_network_count( $network, $count ) {
+		if ( empty ( self::$options ) ) {
+			self::get_options();
+		}
+
+		$key = "{$network}_follow_count";
+
+		self::$options[$key] = $count;
 	}
 
 
