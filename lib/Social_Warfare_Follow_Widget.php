f618c54a056edf954ffde7e3e155d7abf74d6597
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-28 13:37:31 -0500

    Temp commenting out Facebook for testing.

--- lib/Social_Warfare_Follow_Widget.php
+++ lib/Social_Warfare_Follow_Widget.php
@@ -89,7 +89,7 @@ class Social_Warfare_Follow_Widget extends Social_Warfare_Addon {
 	 */
 	public function init_networks() {
 		$networks = array(
-			'Facebook',
+			// 'Facebook',
 			'Pinterest',
 			'Reddit',
 			'Twitter',
