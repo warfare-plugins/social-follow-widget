4ffe6b5f805c439d8e8fa274946a881515267504
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-02-12 11:21:38 -0500

    More debugging

--- lib/networks/SWFW_Vimeo.php
+++ lib/networks/SWFW_Vimeo.php
@@ -64,9 +64,6 @@ class SWFW_Vimeo extends SWFW_Follow_Network {
 		$url = "https://api.vimeo.com/users/{$this->username}/followers";
 		$headers = array('Content-Type: application/json' , "Authorization: Bearer $access_token" );
 		$this->response = SWP_CURL::file_get_contents_curl( $url, $headers );
-		error_log('Response for Vimeo with username ' . $this->username );
-		error_log('type of response before parsing' . gettype($this->response ));
-		error_log(var_dump( $this->response, 1));
 	}
 
 
@@ -83,9 +80,10 @@ class SWFW_Vimeo extends SWFW_Follow_Network {
 			return "000";
 		}
 
-		if ( is_string($this->response)) {
+		if ( is_string( $this->response ) ) {
 			$this->response = json_decode( $this->response );
-			
+			error_log('Parsed response', var_export($this->response, 1));
+			error_log('Just the total: ', var_export($this->response->total));
 		}
 
 
